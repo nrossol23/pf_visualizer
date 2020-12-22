577889f9901035e18955137cea449e13ddd52d6b
Nathan Alexander Rossol <nrossol@umich.edu>
2020-12-22 13:28:28 -0500

    Added A*
    Implemented the A* pathfinding algorithm and added it to user selection.

--- pfv.py
+++ pfv.py
@@ -11,6 +11,12 @@ class visualizer:
             self.p = (-1,-1) #parent node
             self.k = False #discovered
             self.w = False #is wall?
+            
+            #for A*:
+            # f(n) = g(n) + h(n) 
+            #don't need to store h(n), can easily be calculated (it is an estimate)
+            #by the same logic, we can easily calculate f(n) instead of storing it
+            self.g_score = math.inf
 
     def __init__(self):
         #colors:
@@ -25,7 +31,7 @@ class visualizer:
         #initialize pygame
         pygame.init()
         pygame.display.init()
-        pygame.display.set_caption("Path Finding Visualizer")
+        pygame.display.set_caption("Path Finding Visualizer - By Nathan Rossol")
 
         #GUI variables
         self.screen_width = 1098
@@ -81,20 +87,27 @@ class visualizer:
                 word_surface = self.font.render('Choose End Point', False, self.BLUE)
                 self.screen.blit(word_surface, (self.screen_width/2 - 110, self.screen_height + self.menu_height/2))
             else:
-                #set up the buttons for choosing algorithm:
-                button1 = pygame.draw.rect(self.screen, self.BLUE, (0 + self.screen_width/5 - 20, self.screen_height + self.menu_height/2, 175, 40), 0)
-                button2 = pygame.draw.rect(self.screen, self.BLUE, (0 + (self.screen_width/5) * 2 - 20, self.screen_height + self.menu_height/2, 175, 40), 0)
-                button3 = pygame.draw.rect(self.screen, self.BLUE, (0 + (self.screen_width/5) * 3 - 20, self.screen_height + self.menu_height/2, 175, 40), 0)
-                button4 = pygame.draw.rect(self.screen, self.BLUE, (0 + (self.screen_width/5) * 4 - 20, self.screen_height + self.menu_height/2, 175, 40), 0)
+                word_surface = self.font.render('Click To Add Walls', False, self.BLUE)
+                self.screen.blit(word_surface, (self.screen_width/2 - 110, self.screen_height + self.menu_height/10))
 
-                word_surface = self.font.render('Run BFS', False, self.WHITE)
-                self.screen.blit(word_surface, (0 + self.screen_width/5, self.screen_height + self.menu_height/2))
-                word_surface = self.font.render('Run DFS', False, self.WHITE)
-                self.screen.blit(word_surface, (0 + (self.screen_width/5) * 2, self.screen_height + self.menu_height/2))
-                word_surface = self.font.render('Run Dijkstra', False, self.WHITE)
-                self.screen.blit(word_surface, (0 + (self.screen_width/5) * 3, self.screen_height + self.menu_height/2))
+                #set up the buttons for choosing algorithm:
+                button1 = pygame.draw.rect(self.screen, self.BLUE, (0 + self.screen_width/6 - 80, self.screen_height + self.menu_height/2, 175, 40), 0)
+                button2 = pygame.draw.rect(self.screen, self.BLUE, (0 + (self.screen_width/6) * 2 - 80, self.screen_height + self.menu_height/2, 175, 40), 0)
+                button3 = pygame.draw.rect(self.screen, self.BLUE, (0 + (self.screen_width/6) * 3 - 80, self.screen_height + self.menu_height/2, 175, 40), 0)
+                button4 = pygame.draw.rect(self.screen, self.BLUE, (0 + (self.screen_width/6) * 4 - 80, self.screen_height + self.menu_height/2, 175, 40), 0)
+                button5 = pygame.draw.rect(self.screen, self.BLUE, (0 + (self.screen_width/6) * 5 - 80, self.screen_height + self.menu_height/2, 175, 40), 0)
+
+
+                word_surface = self.font.render('BFS', False, self.WHITE)
+                self.screen.blit(word_surface, (0 + self.screen_width/6 - 60, self.screen_height + self.menu_height/2))
+                word_surface = self.font.render('DFS', False, self.WHITE)
+                self.screen.blit(word_surface, (0 + (self.screen_width/6) * 2 - 60, self.screen_height + self.menu_height/2))
+                word_surface = self.font.render('Dijkstra', False, self.WHITE)
+                self.screen.blit(word_surface, (0 + (self.screen_width/6) * 3 - 60, self.screen_height + self.menu_height/2))
+                word_surface = self.font.render('A*', False, self.WHITE)
+                self.screen.blit(word_surface, (0 + (self.screen_width/6) * 4 - 60, self.screen_height + self.menu_height/2))
                 word_surface = self.font.render('Restart', False, self.WHITE)
-                self.screen.blit(word_surface, (0 + (self.screen_width/5) * 4, self.screen_height + self.menu_height/2))
+                self.screen.blit(word_surface, (0 + (self.screen_width/6) * 5 - 60, self.screen_height + self.menu_height/2))
 
             pygame.display.update()
             self.clock.tick(60)
@@ -116,8 +129,6 @@ class visualizer:
                     else: #draw walls:
                         pygame.draw.rect(self.screen, self.BLACK, (x*(self.box_size + self.margin), y*(self.box_size + self.margin), self.box_size, self.box_size), 0)
                         self.data[x][y].w = True
-                        print(x)
-                        print(y)
 
                 elif start_chosen and end_chosen:
                     #run proper algorithm
@@ -128,6 +139,8 @@ class visualizer:
                     elif button3.collidepoint(pos):
                         self.runDijkstra(xi, yi, xf, yf)
                     elif button4.collidepoint(pos):
+                        self.runAStar(xi, yi, xf, yf)
+                    elif button5.collidepoint(pos):
                         self.resetVisualizer()
                         start_chosen = False
                         end_chosen = False
@@ -252,6 +265,37 @@ class visualizer:
             adj = self.getAdj(temp[0], temp[1])
 
             for p in adj: queue.append(p)      
+
+    #run A* Path-finding algorithm
+    #Very similar to Dijkstra's, but uses a heuristic to determine which nodes to test
+    def runAStar(self, xi, yi, xf, yf):
+        pq = [] #our pq is now based upon the distance given from the heuristic
+        start = (xi,yi)
+        heapq.heappush(pq, (0, start))
+        self.data[xi][yi].g_score = 0
+        while len(pq) != 0:
+            v0 = heapq.heappop(pq)[1]
+            if v0[0] == xf and v0[1] == yf:
+                self.displayPath(xi, yi, xf, yf)
+                return
+            if self.data[v0[0]][v0[1]].k == False:
+
+                if v0[0] != xi or v0[1] != yi:
+                    pygame.draw.rect(self.screen, self.BLUE, (v0[0]*(self.box_size + self.margin), v0[1]*(self.box_size + self.margin), self.box_size, self.box_size), 0)
+                    pygame.display.update()
+                    self.clock.tick(60)                
+
+                self.data[v0[0]][v0[1]].k = True
+                adj = self.getAdj(v0[0], v0[1])
+                for v1 in adj:
+                    tentative_g_score = self.data[v0[0]][v0[1]].g_score + self.getDist(v0[0],v0[1],v1[0],v1[1])
+                    if tentative_g_score < self.data[v1[0]][v1[1]].g_score:
+                        self.data[v1[0]][v1[1]].p = (v0[0], v0[1])
+                        self.data[v1[0]][v1[1]].g_score = tentative_g_score
+                        toPush = (v1[0], v1[1])
+                        f_score = tentative_g_score + self.getDist(v1[0], v1[1], xf, yf)
+                        heapq.heappush(pq, (f_score, toPush))
+
 
 
 #Run program:
